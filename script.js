b6338489f57a23b2e8a9def99d11d0c7a719abcc
Leonardo <leonardomullerdelima@pm.me>
2025-06-24 14:59:20 -0300

    feat: add initial gameBoard function

--- script.js
+++ script.js
@@ -9,5 +9,45 @@ function createPlayer(name, mark) {
     return points;
   }
 
-  return { getPoints, name, mark, addOnePoint };
+  return {
+    name,
+    mark,
+    getPoints,
+    addOnePoint,
+  };
+}
+
+function gameBoard() {
+  let board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  function updateBoard(row, column, mark) {
+    if (row < 0 || row > 2 || column < 0 || column > 2) {
+      console.log("Invalid board position.");
+      return;
+    }
+
+    board[row][column] = mark;
+  }
+
+  function getBoard() {
+    return board;
+  }
+
+  function resetBoard() {
+    board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+  }
+
+  return {
+    getBoard,
+    changeBoard,
+    resetBoard,
+  };
 }
