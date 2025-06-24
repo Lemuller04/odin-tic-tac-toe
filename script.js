cd59353564936a24d02791f5a9f074ef6355d51e
Leonardo <leonardomullerdelima@pm.me>
2025-06-24 14:41:57 -0300

    feat: add initial createPlayer function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!");
+function createPlayer(name, mark) {
+  let points = 0;
+
+  function addOnePoint() {
+    points++;
+  }
+
+  function getPoints() {
+    return points;
+  }
+
+  return { getPoints, name, mark, addOnePoint };
+}
